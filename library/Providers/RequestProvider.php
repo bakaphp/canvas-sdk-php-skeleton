902ffa99e5a2b9499a59fd23fcb1ff1b396f22f5
Rafael White <rwhite@mctekk.com>
2020-02-17 17:05:53 -0400

    Add custom request provider

--- library/Providers/RequestProvider.php
+++ library/Providers/RequestProvider.php
@@ -5,7 +5,23 @@ declare(strict_types=1);
 namespace Gewaer\Providers;
 
 use Canvas\Providers\RequestProvider as CanvasRequestProvider;
+use Gewaer\Http\Request;
+use Gewaer\Http\SwooleRequest;
+use Phalcon\Di\ServiceProviderInterface;
+use Phalcon\DiInterface;
+use function Gewaer\Core\isSwooleServer;
 
-class RequestProvider extends CanvasRequestProvider
+class RequestProvider implements ServiceProviderInterface
 {
+    /**
+     * @param DiInterface $container
+     */
+    public function register(DiInterface $container)
+    {
+        if (isSwooleServer()) {
+            $container->setShared('request', new SwooleRequest());
+        } else {
+            $container->setShared('request', new Request());
+        }
+    }
 }
