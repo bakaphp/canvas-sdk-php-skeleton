2975e2a67b5ccb10794ea79a17db441f876da318
Max Castro <max@mctekk.com>
2020-04-09 12:59:28 -0400

    Update RequestProvider.php

--- library/Providers/RequestProvider.php
+++ library/Providers/RequestProvider.php
@@ -5,22 +5,7 @@ declare(strict_types=1);
 namespace Gewaer\Providers;
 
 use Canvas\Providers\RequestProvider as CanvasRequestProvider;
-use Phalcon\DiInterface;
-use Gewaer\Http\Request;
-use Gewaer\Http\SwooleRequest;
-use function Gewaer\Core\isSwooleServer;
 
 class RequestProvider extends CanvasRequestProvider
 {
-    /**
-     * @param DiInterface $container
-     */
-    public function register(DiInterface $container)
-    {
-        if (isSwooleServer()) {
-            $container->setShared('request', new SwooleRequest());
-        } else {
-            $container->setShared('request', new Request());
-        }
-    }
 }
