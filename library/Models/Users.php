975978740c71000313865597d5c9a41c6a9d5b11
Max Castro <max@mctekk.com>
2020-04-09 16:50:26 -0400

    Update Users.php

--- library/Models/Users.php
+++ library/Models/Users.php
@@ -3,37 +3,15 @@ declare(strict_types=1);
 
 namespace Gewaer\Models;
 
-use Canvas\Models\Users as CanvasUsers;
-use Kanvas\Sdk\Traits\FileSystemModelTrait;
-use Kanvas\Sdk\Traits\CustomFieldsTrait;
-use Kanvas\Sdk\Traits\PermissionsTrait;
+use Kanvas\Sdk\Users as CanvasUsers;
 
 /**
  * Class Users.
  *
  * @package Canvas\Models
  *
- * @property Users $user
- * @property Config $config
- * @property Apps $app
- * @property Companies $defaultCompany
- * @property \Phalcon\Di $di
  */
-class Users extends BaseModel
+class Users extends CanvasUsers
 {
-    use PermissionsTrait;
-    use FileSystemModelTrait;
-    use CustomFieldsTrait;
 
-    /**
-     * Upload Files.
-     *
-     * @todo move this to the baka class
-     * @return void
-     */
-    public function afterSave()
-    {
-        // parent::afterSave();
-        $this->associateFileSystem();
-    }
 }
