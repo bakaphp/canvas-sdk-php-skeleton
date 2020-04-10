1af83dfbfc78f4a34ec05a9c817586c86da9084f
Max Castro <max@mctekk.com>
2020-04-10 00:37:35 -0400

    Create 20200408052401_custom_fields.php

---
+++ storage/db/migrations/20200408052401_custom_fields.php
@@ -0,0 +1,111 @@
+<?php
+
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class CustomFields extends Phinx\Migration\AbstractMigration
+{
+    public function change()
+    {
+        $this->table('custom_fields', [
+                'id' => false,
+                'primary_key' => ['id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'latin1',
+                'collation' => 'latin1_swedish_ci',
+                'comment' => '',
+                'row_format' => 'DYNAMIC',
+            ])
+            ->addColumn('id', 'biginteger', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_BIG,
+            ])
+            ->addColumn('companies_id', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'after' => 'id',
+            ])
+            ->addColumn('users_id', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'after' => 'companies_id',
+            ])
+            ->addColumn('model_name', 'string', [
+                'null' => false,
+                'limit' => 255,
+                'collation' => 'latin1_swedish_ci',
+                'encoding' => 'latin1',
+                'after' => 'users_id',
+            ])
+            ->addColumn('entity_id', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'after' => 'model_name',
+            ])
+            ->addColumn('name', 'string', [
+                'null' => false,
+                'limit' => 255,
+                'collation' => 'latin1_swedish_ci',
+                'encoding' => 'latin1',
+                'after' => 'entity_id',
+            ])
+            ->addColumn('label', 'string', [
+                'null' => false,
+                'limit' => 255,
+                'collation' => 'latin1_swedish_ci',
+                'encoding' => 'latin1',
+                'after' => 'name',
+            ])
+            ->addColumn('value', 'text', [
+                'null' => false,
+                'limit' => MysqlAdapter::TEXT_LONG,
+                'collation' => 'latin1_swedish_ci',
+                'encoding' => 'latin1',
+                'after' => 'label',
+            ])
+            ->addColumn('created_at', 'datetime', [
+                'null' => false,
+                'after' => 'value',
+            ])
+            ->addColumn('updated_at', 'datetime', [
+                'null' => true,
+                'default' => null,
+                'after' => 'created_at',
+            ])
+            ->addColumn('is_deleted', 'integer', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_TINY,
+                'after' => 'updated_at',
+            ])
+            ->addIndex(['companies_id'], [
+                'name' => 'companies_id',
+                'unique' => false,
+            ])
+            ->addIndex(['users_id'], [
+                'name' => 'users_id',
+                'unique' => false,
+            ])
+            ->addIndex(['model_name'], [
+                'name' => 'model_name',
+                'unique' => false,
+            ])
+            ->addIndex(['entity_id'], [
+                'name' => 'entity_id',
+                'unique' => false,
+            ])
+            ->addIndex(['name'], [
+                'name' => 'name',
+                'unique' => false,
+            ])
+            ->addIndex(['label'], [
+                'name' => 'label',
+                'unique' => false,
+            ])
+            ->addIndex(['companies_id', 'model_name', 'entity_id'], [
+                'name' => 'companies_id_model_name_entity_id',
+                'unique' => false,
+            ])
+            ->create();
+    }
+}
